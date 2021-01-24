67ed68aa5851d79a8926acdfd1d7570f5f8fc9ac
Mateusz Hadrys <m.hadrys.95@gmail.com>
2021-01-24 11:23:19 +0100

    Feat: Add blog template and generate pages

---
+++ src/templates/BlogTemplate/BlogTemplate.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+
+import { Content } from 'components/atoms/Content/Content';
+import { Text } from 'components/atoms/Text/Text';
+
+import { Wrapper, Flex, InnerWrapper, Img } from './BlogTemplate.style';
+
+const BlogTemplate = ({
+  data: {
+    blogPage: {
+      title,
+      image: { url, alt },
+    },
+  },
+}) => (
+  <Wrapper>
+    <Content>
+      <Flex>
+        <InnerWrapper>
+          <Text isTitle isMedium color="white">
+            {title}
+          </Text>
+        </InnerWrapper>
+        <InnerWrapper>
+          <Img src={url} alt={alt} />
+        </InnerWrapper>
+      </Flex>
+    </Content>
+  </Wrapper>
+);
+
+export const query = graphql`
+  query BlogQuery($locale: String!) {
+    blogPage: datoCmsBlogPage(locale: { eq: $locale }) {
+      title
+      image {
+        url
+        alt
+      }
+    }
+  }
+`;
+
+BlogTemplate.propTypes = {
+  data: PropTypes.shape({
+    blogPage: PropTypes.shape({
+      title: PropTypes.string,
+      image: PropTypes.objectOf(PropTypes.string),
+    }),
+  }).isRequired,
+};
+
+export default BlogTemplate;
