808619c61cc30a4611bb4472ea2c2c0ad42f415f
Mateusz Hadrys <m.hadrys.95@gmail.com>
2021-01-27 18:57:53 +0100

    Feat: Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,6 @@ const path = require('path');
 require('dotenv').config();
 
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
-  },
   flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -36,19 +30,6 @@ module.exports = {
         },
       },
     },
-
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-    //   },
-    // },
     {
       resolve: 'gatsby-plugin-root-import',
       options: {
--- src/components/organisms/SEO/SEO.js
+++ src/components/organisms/SEO/SEO.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { HelmetDatoCms } from 'gatsby-source-datocms';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const SEO = ({ seo, children, ...props }) => (
-  // console.log(props);
+const query = graphql`
+  {
+    datoCmsSite {
+      faviconMetaTags {
+        ...GatsbyDatoCmsFaviconMetaTags
+      }
+    }
+  }
+`;
 
-  <HelmetDatoCms seo={seo} {...props}>
-    {children}
-  </HelmetDatoCms>
-);
+const SEO = ({ seo, children, ...props }) => {
+  const {
+    datoCmsSite: { faviconMetaTags },
+  } = useStaticQuery(query);
+
+  return (
+    <HelmetDatoCms seo={seo} favicon={faviconMetaTags} {...props}>
+      {children}
+    </HelmetDatoCms>
+  );
+};
 
 SEO.propTypes = {
   seo: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.object)),
